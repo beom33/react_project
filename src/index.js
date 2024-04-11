19bc5ebc39e76f213a5527497c15b41111d09f97
beom33 <byby5546@naver.com>
2024-04-11 12:23:19 +0900

    no message

--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
-import'./i18n';
+import { HelmetProvider } from 'react-helmet-async';
+import './i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <helmetProvider>
-           <App />
-    </helmetProvider>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>,
 );
 
